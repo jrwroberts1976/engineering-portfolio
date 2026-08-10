90566cb17976dbe5154dd72d2e5c4e4e5b9c3f4a
James Roberts <jrwroberts1976@gmail.com>
2026-08-10 05:09:42 +0100

    Add historical BirdNET activity sample

---
+++ src/data/birdActivity.ts
@@ -0,0 +1,80 @@
+export interface BirdActivitySpecies {
+  code: string;
+  name: string;
+  counts: number[];
+}
+
+export const birdActivityDate = 'Mon, 3 Aug 2026';
+
+export const birdActivity: BirdActivitySpecies[] = [
+  {
+    code: 'ER',
+    name: 'European Robin',
+    counts: [0, 0, 0, 0, 21, 21, 14, 38, 8, 0, 18, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  },
+  {
+    code: 'CW',
+    name: 'Common Wood-Pigeon',
+    counts: [0, 0, 0, 0, 0, 13, 1, 1, 0, 8, 9, 1, 1, 1, 3, 16, 1, 7, 5, 1, 1, 0, 0, 0],
+  },
+  {
+    code: 'EM',
+    name: 'Eurasian Magpie',
+    counts: [0, 0, 0, 0, 0, 0, 1, 2, 2, 7, 4, 6, 0, 0, 0, 0, 0, 0, 22, 6, 12, 0, 0, 0],
+  },
+  {
+    code: 'EG',
+    name: 'European Greenfinch',
+    counts: [0, 0, 0, 0, 0, 0, 15, 4, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  },
+  {
+    code: 'EN',
+    name: 'Eurasian Nuthatch',
+    counts: [0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  },
+  {
+    code: 'DU',
+    name: 'Dunnock',
+    counts: [0, 0, 0, 0, 0, 0, 11, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  },
+  {
+    code: 'EJ',
+    name: 'Eurasian Jackdaw',
+    counts: [0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 5, 1, 0, 0, 0, 0, 0, 0, 0],
+  },
+  {
+    code: 'EB',
+    name: 'Eurasian Blue Tit',
+    counts: [0, 0, 0, 0, 0, 0, 0, 6, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  },
+  {
+    code: 'CT',
+    name: 'Coal Tit',
+    counts: [0, 0, 0, 0, 0, 0, 3, 1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  },
+  {
+    code: 'EH',
+    name: 'European Herring Gull',
+    counts: [0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0],
+  },
+  {
+    code: 'TO',
+    name: 'Tawny Owl',
+    counts: [1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  },
+  {
+    code: 'GT',
+    name: 'Great Tit',
+    counts: [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  },
+  {
+    code: 'YG',
+    name: 'Yellow-legged Gull',
+    counts: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  },
+  {
+    code: 'EW',
+    name: 'Eurasian Wren',
+    counts: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  },
+];
